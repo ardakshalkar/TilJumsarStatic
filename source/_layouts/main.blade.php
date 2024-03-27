59dc202ad9db0a25f3e32f9f76c26b6b6ad9c157
Ardak Shalkarbayuly <ardak.shalkar@gmail.com>
2024-03-27 13:39:45 +0500

    version 1

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -7,9 +7,29 @@
         <meta name="description" content="{{ $page->description }}">
         <title>{{ $page->title }}</title>
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
         <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
     </head>
-    <body class="text-gray-900 font-sans antialiased">
-        @yield('body')
+    <body class="text-gray-900 font-sans antialiased flex">
+        <div class="flex flex-col min-h-screen sm:container">
+            <div class="">
+                <div class="container-sm sm:mx-auto max-w-screen-xl pt-5">
+                    @include('partials.header')
+                </div>
+            </div>
+            <div class="flex-grow flex">
+                <div class="max-w-screen-xl w-screen mx-auto">
+                    @yield('body')
+                </div>
+            </div>
+            <div class="flex-shrink bg-gray-700">
+                <div class="max-w-screen-xl mx-auto">
+                    @include('partials.footer')
+                </div>
+            </div>
+        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     </body>
 </html>
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,75 @@
 @extends('_layouts.main')
 
 @section('body')
-<div class="p-8">
-    <h1 class="text-3xl font-bold">Hello world!</h1>
+<div class="w-[1284px] h-[601px] relative">
+  <div class="w-[917px] h-[601px] left-0 top-0 absolute">
+    <div class="w-[917px] h-[601px] left-0 top-0 absolute">
+      <img class=" left-[36.27px] top-0 absolute" src="assets/images/hero.png" />
+    </div>
+    <div class="origin-top-left w-[253px] h-[222px] left-[587px] top-[347px] absolute">
+      <div class="w-[253px] h-[222px] left-[-253px] top-0 absolute bg-white rounded-xl shadow"></div>
+      <div class="w-[154px] h-12 left-[-209px] top-[72px] absolute">
+        <div class="w-12 h-12 left-0 top-0 absolute">
+          <div class="w-12 h-12 left-0 top-0 absolute bg-purple-100 rounded-full"></div>
+          <div class="left-[14px] top-[9px] absolute text-fuchsia-700 text-lg font-semibold font-['Poppins'] leading-[30px]">A1</div>
+        </div>
+        <div class="left-[64px] top-[9px] absolute text-gray-700 text-lg font-semibold font-['Inter'] leading-[30px]">A1 деңгей</div>
+      </div>
+      <div class="w-[157px] h-12 left-[-209px] top-[142px] absolute">
+        <div class="w-12 h-12 left-0 top-0 absolute">
+          <div class="w-12 h-12 left-0 top-0 absolute bg-indigo-100 rounded-full"></div>
+          <div class="left-[12px] top-[9px] absolute text-blue-500 text-lg font-semibold font-['Poppins'] leading-[30px]">A2</div>
+        </div>
+        <div class="left-[64px] top-[9px] absolute text-gray-700 text-lg font-semibold font-['Inter'] leading-[30px]">A2 деңгей</div>
+      </div>
+      <div class="left-[-178px] top-[22px] absolute text-gray-700 text-lg font-extrabold font-['Inter'] leading-[30px]">Деңгейлер</div>
+    </div>
+  </div>
+  <div class="w-[547px] h-[423px] left-[737px] top-[21px] absolute">
+    <div class="w-[354px] h-[54px] left-0 top-[369px] absolute">
+    <div class="w-96 h-14 relative">
+        <div class="left-0 top-0 absolute">
+            <span class="text-gray-700 text-5xl font-bold font-['Inter'] leading-10">350</span><span class="text-gray-700 text-5xl font-bold font-['Sen'] leading-10"> </span><span class="text-gray-700 text-lg font-semibold font-['Inter'] leading-loose">Студент</span>
+        </div>
+        <div class="left-[201px] top-0 absolute">
+            <span class="text-gray-700 text-5xl font-bold font-['Inter'] leading-10">70+ </span><span class="text-gray-700 text-lg font-semibold font-['Inter'] leading-loose">Cабақ</span></div>
+        </div>
+      <!--<div class="left-0 top-0 absolute"><span style="text-gray-700 text-[44px] font-bold font-['Inter'] leading-[54px]">350</span><span style="text-gray-700 text-[44px] font-bold font-['Sen'] leading-[54px]"> </span><span style="text-gray-700 text-lg font-semibold font-['Inter'] leading-[30px]">Студент</span></div>
+      <div class="left-[201px] top-0 absolute"><span style="text-gray-700 text-[44px] font-bold font-['Inter'] leading-[54px]">70+ </span><span style="text-gray-700 text-lg font-semibold font-['Inter'] leading-[30px]">Cабақ</span></div>-->
+    </div>
+    <div class="text-[58px] font-bold">
+        <span>Қазақ тілін</span><br/>
+        <span class="text-orange-400">til-jumsar.kz</span><br/>
+        <span>арқылы үйреніңіз</span>
+    </div>
+    <!--<div class="w-[547px]"><span style="text-gray-700 text-6xl font-bold font-['Inter'] capitalize leading-[80px]">Қазақ тілін X<br/></span><span style="text-orange-400 text-6xl font-bold  leading-[80px]">til-jumsar.kz<br/></span><span style="text-gray-700 text-6xl font-bold font-['Inter'] capitalize leading-[80px]">арқылы үйреніңіз</span></div>-->
+    <!--
+    <div class="w-[547px] left-0 top-0 absolute"><span style="text-gray-700 text-[58px] font-bold font-['Inter'] capitalize leading-[80px]">Қазақ тілін <br/></span><span style="text-orange-400 text-[58px] font-bold font-['Inter'] capitalize leading-[80px]">til-jumsar.kz<br/></span><span style="text-gray-700 text-[58px] font-bold font-['Inter'] capitalize leading-[80px]">арқылы үйреніңіз</span></div>-->
+  </div>
+</div>
+<div class="py-10">
+    <div class="my-5 self-stretch text-center text-gray-700 text-4xl font-semibold font-['Inter'] leading-10">Біз ұсынатын қазақ тілін үйрену тәсілдері</div>
+    <div class="my-10 flex flex-row justify-around">
+        <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Интерактивтілік</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Геймификация элементтері бар бейнероликтер, аудиофайлдар</p>
+        </div>
+        <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Қызық</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Ойын элементтері бар сабақтар, бейне сабақтар, фото және медиа</p>
+        </div>
+        <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Дербестік</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Тапсырмалардың өтуіне қарай сізге ұпай беріледі</p>
+        </div>
+    </div>
+</div>
+<div class="w-96 h-96 relative">
+  <div class="w-96 left-[494px] top-[113px] absolute text-gray-700 text-2xl font-normal font-['Inter'] leading-10">Спасибо порталу qazedu.kz! Теперь я могу свободно разговаривать на казахском. Ну, не сочиняю стихи, но уже могу говорить на бытовом уровне</div>
+  <div class="w-32 h-40 left-[135px] top-[96px] absolute">
+    <div class="left-0 top-[128px] absolute text-zinc-800 text-sm font-normal font-['Inter'] leading-relaxed">Инженер</div>
+    <div class="left-0 top-[90px] absolute text-gray-700 text-2xl font-bold font-['Inter'] leading-loose">Peter Moor</div>
+    <img class="w-16 h-16 left-0 top-0 absolute rounded-full" src="https://via.placeholder.com/72x72" />
+  </div>
 </div>
 @endsection
